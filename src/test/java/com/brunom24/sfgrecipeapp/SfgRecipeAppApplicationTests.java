807171ce0cc3e14680a8a2e042a3c103f20e81ad
BrunoM24 <brunocostamatos.24@gmail.com>
2019-11-21 12:25:13 +0000

    Added the base project

---
+++ src/test/java/com/brunom24/sfgrecipeapp/SfgRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunom24.sfgrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
